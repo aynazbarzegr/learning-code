5f0ca48b48eb46c490628a04e5366a78d8c7c3c5
AYNAZ <ainazzbarzgrr@gmail.com>
2026-06-01 23:30:50 +0330

    Add files via upload

---
+++ mashin.py
@@ -0,0 +1,11 @@
+num1 = float(input("enter number :"))
+amalgar = input(("+,-,*,/ :"))
+num2 = float(input("enter number :"))
+if amalgar == "+":
+    print(num1+num2)
+elif amalgar == "-":
+    print(num1-num2)
+elif amalgar == "*":
+    print(num1*num2)
+elif amalgar == "/":
+    print(num1/num2)
