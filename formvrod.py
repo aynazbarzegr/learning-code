1782ec7e5ecc736ade9da2dc3f163ae0353d1d07
AYNAZ <ainazzbarzgrr@gmail.com>
2026-06-01 23:26:10 +0330

    Add files via upload

---
+++ formvrod.py
@@ -0,0 +1,6 @@
+username = input("enter username:")
+pasword =input("enter pasword:")
+if username == "aynaz" and pasword == "1386":
+    print("login succesful")
+else:
+    print("username or pasword is incorrect")
